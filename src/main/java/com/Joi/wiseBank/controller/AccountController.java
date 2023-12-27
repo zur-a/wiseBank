ae63b75870ad44d62745bb8f7dc760f06622fe8c
zura <jur.and.jr42@gmail.com>
2023-12-27 10:19:18 -0300

    Created myAccount get request

--- src/main/java/com/Joi/wiseBank/controller/AccountController.java
+++ src/main/java/com/Joi/wiseBank/controller/AccountController.java
@@ -1,4 +1,12 @@
 package com.Joi.wiseBank.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AccountController {
+    @GetMapping("/myAccount")
+    public String getAccountDetails() {
+        return "Welcome to Wise bank";
+    }
 }
