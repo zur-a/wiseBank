17ae170cdf9e5af4cf421b95e4fd0c0f94da547a
zura <jur.and.jr42@gmail.com>
2023-12-27 10:25:05 -0300

    Created LoanController class and loans get request

--- src/main/java/com/Joi/wiseBank/controller/LoanController.java
+++ src/main/java/com/Joi/wiseBank/controller/LoanController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AccountController {
-    @GetMapping("/account")
-    public String accountDetails() {
-        return "Welcome to Wise bank";
+public class LoanController {
+    @GetMapping("/loans")
+    public String loanDetails() {
+        return "Loans details are provided here";
     }
 }
