265ea337c13786f1ebe62653ad07515160c2fdfa
zura <jur.and.jr42@gmail.com>
2023-12-27 10:04:33 -0300

    Initial commit

---
+++ src/main/java/com/Joi/wiseBank/WiseBankApplication.java
@@ -0,0 +1,13 @@
+package com.Joi.wiseBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Joi/wiseBank/WiseBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Joi.wiseBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
