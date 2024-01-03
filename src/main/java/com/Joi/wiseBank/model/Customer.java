28e233718e1b0b353e939945846f7ce008f091fc
zura <jur.and.jr42@gmail.com>
2024-01-03 10:29:13 -0300

    Created Customer class in model package

---
+++ src/main/java/com/Joi/wiseBank/model/Customer.java
@@ -0,0 +1,56 @@
+package com.Joi.wiseBank.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_sequence",
+            sequenceName = "customer_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.AUTO,
+            generator = "customer_sequence"
+    )
+    private int id;
+    @Column
+    private String email;
+    @Column
+    private String pwd;
+    @Column
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
