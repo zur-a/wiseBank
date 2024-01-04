ea8a9379225cf6bc6c24cdb4a90be718cf4754b9
zura <jur.and.jr42@gmail.com>
2024-01-04 10:40:41 -0300

    Added BCrypt version and strength

--- src/main/java/com/Joi/wiseBank/config/SecurityConfig.java
+++ src/main/java/com/Joi/wiseBank/config/SecurityConfig.java
@@ -27,6 +27,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2Y, 21);
     }
 }
