e7b7573891c115c49934b87b06ac38428141773a
zura <jur.and.jr42@gmail.com>
2024-01-11 16:37:51 -0300

    Refactored getAccountDetails in AccountController to reflect model and repository changes

--- src/main/java/com/Joi/wiseBank/controller/AccountController.java
+++ src/main/java/com/Joi/wiseBank/controller/AccountController.java
@@ -1,12 +1,24 @@
 package com.Joi.wiseBank.controller;
 
+import com.Joi.wiseBank.Repository.AccountsRepository;
+import com.Joi.wiseBank.model.Accounts;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+    @Autowired
+    private AccountsRepository repository;
+
     @GetMapping("/account")
-    public String accountDetails() {
-        return "Welcome to Wise bank";
+    public Accounts getAccountDetails(@RequestParam int id) {
+        Accounts accounts = repository.findByCustomerId(id);
+        if (accounts != null) {
+            return accounts;
+        } else {
+            return null;
+        }
     }
 }
